544d9c421a4e0bdd98875ea40c50fe10119d7d0e
CS252 <cs252@cs.purdue.edu>
2019-10-19 09:35:42 -0400

    Changed g_prod_str.

--- part1.c
+++ part1.c
@@ -8,7 +8,7 @@
 
 // The producers add the characters in g_prod_str to the g_buffer, in order.
 
-char *g_prod_str = "Hello, World!";
+char *g_prod_str = "Do. Or do not. There is no try.";
 
 bounded_buffer g_buffer;
 
