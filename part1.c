c4228766c42b3dee3f544a6ffe9202068d0a427f
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-10-27 15:49:19 -0400

    Commit lab 4

--- part1.c
+++ part1.c
@@ -41,8 +41,7 @@ int enqueue(char c) {
   sem_wait(&g_full_sem);
   pthread_mutex_lock(&g_buffer_mutex);
 
-  char *check = NULL;
-  check = strchr(check, c);
+  char *check = strchr(g_buffer.buf, c);
   if (check == NULL) {
     pthread_mutex_unlock(&g_buffer_mutex);
     return SUCCESS;
